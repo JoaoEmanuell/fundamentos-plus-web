3bd389549d2a096dc6c438f5bf5ff2fccf2fa579
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-11-17 17:04:46 -0300

    feat: Cycle component and Green button component

---
+++ web/src/components/body/Header/Cycles.tsx
@@ -0,0 +1,31 @@
+import data from '@/json/data.json'
+import { GreenButton } from '@/components/ui/GreenButton'
+import { title } from 'process'
+
+export function Cycles() {
+    const cycles = data['cycles']
+    const cyclesArray = Object.keys(cycles)
+
+    return (
+        <div>
+            <div>
+                {cyclesArray.map((key) => {
+                    const title = cycles[key]['title']
+                    const disable = cycles[key]['unlocked']
+                    return (
+                        <a key={key} href={`/cycle/${key}`}>
+                            <div className="mb-4 flex justify-start">
+                                {disable ? (
+                                    <GreenButton text={key} enable />
+                                ) : (
+                                    <GreenButton text={key}></GreenButton>
+                                )}
+                                <h1 className="py-3 text-base">{title}</h1>
+                            </div>
+                        </a>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
