7d2ae8a7756eae948ae9dd9c05f7a99d235d6355
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-11-19 21:01:13 -0300

    refactor: green ball and text page style

--- web/src/components/body/Lesson/pageStyles/styles/Text.tsx
+++ web/src/components/body/Lesson/pageStyles/styles/Text.tsx
@@ -1,5 +1,9 @@
 import { StyleInterface } from './StyleInterface'
 
 export function TextStyle(props: StyleInterface) {
-    return <p className={`text-center ${props.className}`}>{props.content}</p>
+    return (
+        <p className={`text-justify text-base ${props.className}`}>
+            {props.content}
+        </p>
+    )
 }
