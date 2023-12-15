ff1109fc5eb6ccaa99a421f611cbb21e59223900
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-15 11:44:45 -0300

    feat: cycles page

---
+++ web/src/app/cycles/page.tsx
@@ -0,0 +1,11 @@
+import { Cycles } from '@/components/body/Cycles'
+import { CenterTitle } from '@/components/ui/CenterTitle'
+
+export default function CyclesPage() {
+    return (
+        <main>
+            <CenterTitle text="Ciclos: "></CenterTitle>
+            <Cycles />
+        </main>
+    )
+}
