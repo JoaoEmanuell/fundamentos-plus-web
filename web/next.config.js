dc8a202f2b94536b3601b46d155781a876ead5b7
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-15 10:52:15 -0300

    refactor: next.config.js
    add env

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        PATH_TO_DATA: process.env.PATH_TO_DATA
+    },
+}
 
 module.exports = nextConfig
