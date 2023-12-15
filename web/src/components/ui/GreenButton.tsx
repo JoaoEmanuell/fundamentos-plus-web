dd6525d0a8c4e5c7b5840e751c1f9489407560a0
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-14 21:27:11 -0300

    refactor: GreenButton
    add divClassName

--- web/src/components/ui/GreenButton.tsx
+++ web/src/components/ui/GreenButton.tsx
@@ -1,11 +1,12 @@
 interface GreenButtonProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
     text: string
+    divClassName?: string
 }
 
 export function GreenButton(props: GreenButtonProps) {
     return (
         <div
-            className="mt-4 border border-gray-200 rounded bg-greenButton text-white px-4 cursor-pointer"
+            className={`mt-4 border border-gray-200 rounded bg-greenButton text-white px-4 cursor-pointer ${props.divClassName}`}
             {...props}
         >
             {props.text}
