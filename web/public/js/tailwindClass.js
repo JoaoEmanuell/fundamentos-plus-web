c20869affe8498ed507cf4e4da95af35ad069900
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-18 11:12:16 -0300

    refactor: ImageStyle
    add docs and create a tailwindClass.js

---
+++ web/public/js/tailwindClass.js
@@ -0,0 +1,7 @@
+// custom classes used by tailwindcss
+// this file is used to correct compile
+
+const w_80 = 'w-80'
+const h_64 = 'h-64'
+const md_w_30_rem = 'md:w-[30rem]'
+const md_h_25_rem = 'md:h-[25rem]'
--- web/src/components/body/Lesson/pageStyles/styles/others/ImageStyle.tsx
+++ web/src/components/body/Lesson/pageStyles/styles/others/ImageStyle.tsx
@@ -3,13 +3,18 @@ import { StyleInterface } from '../StyleInterface'
 import Image from 'next/image'
 
 export function ImageStyle(props: StyleInterface) {
+    /* 
+    image_name (save in public/assets/lessons_images) | sizes config (using tailwind to adjust)
+    Note: read the README.md located in lessons_images
+    Note: if do you use custom classes, include in tailwindClass.js located in public/js because if the custom class is include using js, he is not compiled and not work.
+    */
     const contentSplit = props.content.split(' | ')
     const image = `/assets/lessons_images/${contentSplit[0]}`
     const sizes = contentSplit[1]
 
     return (
         <div className={`relative ${sizes}`}>
-            <Image src={image} alt="image" fill className="object-fill" />
+            <Image src={image} alt="image" fill className={`object-fill`} />
         </div>
     )
 }
