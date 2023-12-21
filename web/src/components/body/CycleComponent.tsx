48aee4eaae5c155651a04596f55e3edc70012d88
JoaoEmanuell <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-20 21:27:44 -0300

    refactor: CycleComponent
    use the url to load json

--- web/src/components/body/CycleComponent.tsx
+++ web/src/components/body/CycleComponent.tsx
@@ -1,6 +1,8 @@
-import data from '@/json/data.json'
+'use client'
+
 import { CenterTitle } from '../ui/CenterTitle'
 import { NumberGreenButtonWithText } from '../ui/NumberGreenButtonWithText'
+import { useState, useEffect } from 'react'
 
 interface CycleInterface {
     id: string
@@ -13,25 +15,55 @@ interface lessonInterface {
 }
 
 export function CycleComponent(props: CycleInterface) {
-    try {
-        var status = data['cycles'][props.id]['unlocked']
-    } catch (err) {
+    const [cyclesJson, setCyclesJson] = useState<object | any>()
+    const [cycles, setCycles] = useState<object | any>()
+    const [isLoading, setIsLoading] = useState(true)
+    const [unlockedCycle, setUnlockedCycle] = useState<boolean | null>(null)
+
+    useEffect(() => {
+        const pathToJson = `${process.env.PATH_TO_DATA}/data/data.min.json`
+        fetch(pathToJson, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'force-cache',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                try {
+                    setCyclesJson(data) // set the cycles json
+                    setCycles(data['cycles']) // set the cycles
+                    setUnlockedCycle(data['cycles'][props.id]['unlocked'])
+                    var status = data['cycles'][props.id]['unlocked']
+                    setIsLoading(false)
+                } catch (err) {
+                    setUnlockedCycle(false)
+                } finally {
+                    if (!status) {
+                        setUnlockedCycle(false)
+                    }
+                }
+            })
+    }, []) // Load the cycles in the first execution
+
+    // while the json not load!
+
+    if (isLoading) return <p>Carregando...</p>
+
+    // if cycle is locked or not exists
+
+    if (unlockedCycle === false) {
         return (
             <div>
                 <CenterTitle text="Ciclo não está disponível!" />
             </div>
         )
-    } finally {
-        if (!status) {
-            return (
-                <div>
-                    <CenterTitle text="Ciclo não está disponível!" />
-                </div>
-            )
-        }
     }
-    const title = data['cycles'][props.id]['title']
-    const lessons: lessonInterface[] = data['cycles'][props.id]['lessons']
+
+    const title = cycles[props.id]['title']
+    const lessons: lessonInterface[] = cycles[props.id]['lessons']
     return (
         <div>
             <CenterTitle text={title} />
