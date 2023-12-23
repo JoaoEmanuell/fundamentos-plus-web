3919570ae2b419f2756603bf80d79802d9995e22
JoaoEmanuell <81983803+JoaoEmanuell@users.noreply.github.com>
2023-12-23 11:13:50 -0300

    add favicon

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -15,13 +15,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="pt-br">
-            <head>
-                <link
-                    rel="shortcut icon"
-                    href="/img/logotipo_fundamentos_negativo_fundo.ico"
-                    type="image/x-icon"
-                />
-            </head>
+            <head></head>
             <body className="transition-all duration-500">
                 <noscript>Enable Javascript to use this site!</noscript>
                 <Header />
