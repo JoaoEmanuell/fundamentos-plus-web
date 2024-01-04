10ea18ad217425d2c82f790922c3b4678c0faeb1
JoaoEmanuell <81983803+JoaoEmanuell@users.noreply.github.com>
2024-01-04 10:41:23 -0300

    25 lesson

--- public/js/tailwindClass.js
+++ public/js/tailwindClass.js
@@ -1,13 +1,27 @@
 // custom classes used by tailwindcss
 // this file is used to correct compile
 
+// width
+
 const w_80 = 'w-80'
-const h_64 = 'h-64'
-const md_w_30_rem = 'md:w-[30rem]'
-const md_h_25_rem = 'md:h-[25rem]'
-const w_96 = 'w-89'
-const md_w_35_rem = 'md:w-[35rem]'
+const w_96 = 'w-96'
 const w_21_rem = 'w-[21rem]'
-const h_32 = 'h-32'
+const w_22_rem = 'w-[22rem]'
+const md_w_25_rem = 'md:w-[25rem]'
+const md_w_30_rem = 'md:w-[30rem]'
+const md_w_35_rem = 'md:w-[35rem]'
+const md_w_40_rem = 'md:w-[40rem]'
 const md_w_50_rem = 'md:w-[50rem]'
+const md_w_96 = 'md:w-96'
+
+// height
+
+const h_64 = 'h-64'
+const h_32 = 'h-32'
+const h_48 = 'h-48'
+const md_h_80 = 'md:h-80'
+const md_h_25_rem = 'md:h-[25rem]'
 const md_h_20_rem = 'md:h-[20rem]'
+const md_h_23_rem = 'md:h-[23rem]'
+const md_h_48 = 'md:h-48'
+const md_h_64 = 'md:h-64'
