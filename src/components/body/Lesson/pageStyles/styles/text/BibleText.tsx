c91efd3085a4a2172fd99e9edcc5d9d1ef70157d
JoaoEmanuell <81983803+JoaoEmanuell@users.noreply.github.com>
2024-04-16 17:34:13 -0300

    43 lesson

--- src/components/body/Lesson/pageStyles/styles/text/BibleText.tsx
+++ src/components/body/Lesson/pageStyles/styles/text/BibleText.tsx
@@ -1,11 +1,25 @@
 import { StyleInterface } from '../StyleInterface'
+import { TextBoldStyle } from './TextBold'
 
 import Markdown from 'react-markdown'
 
 export function BibleTextStyle(props: StyleInterface) {
-    return (
-        <em className={`text-lg text-center px-4 ${props.className}`}>
-            <Markdown>{props.content}</Markdown>
-        </em>
-    )
+    /* bible text | reference */
+    if (props.content.includes(' | ')) {
+        const propsSplit = props.content.split(' | ')
+        const bibleText = propsSplit[0]
+        const reference = propsSplit[1]
+        return (
+            <em className={`text-lg text-center px-4 ${props.className}`}>
+                <Markdown>{bibleText}</Markdown>
+                <TextBoldStyle content={reference} className="mt-4" />
+            </em>
+        )
+    } else {
+        return (
+            <em className={`text-lg text-center px-4 ${props.className}`}>
+                <Markdown>{props.content}</Markdown>
+            </em>
+        )
+    }
 }
