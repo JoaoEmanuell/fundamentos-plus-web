5c30b2853e64b951e7c34287eceb4039ebe78c9f
JoaoEmanuell <81983803+JoaoEmanuell@users.noreply.github.com>
2024-05-28 17:49:58 -0300

    100 lesson

--- src/components/ui/NumberGreenButton.tsx
+++ src/components/ui/NumberGreenButton.tsx
@@ -4,11 +4,25 @@ interface GreenButtonInterface {
 }
 
 export function NumberGreenButton(props: GreenButtonInterface) {
+    let styleTextSize
+    if (!isNaN(props.text as unknown as number)) {
+        const number = Number(props.text)
+        if (number >= 100) {
+            styleTextSize = { fontSize: '25px' }
+        }
+    }
     if (props.enable && props.enable === true) {
-        return <span className="green-button-not-connected">{props.text}</span>
+        return (
+            <span className="green-button-not-connected" style={styleTextSize}>
+                {props.text}
+            </span>
+        )
     } else {
         return (
-            <span className="green-button-disabled-not-connected">
+            <span
+                className="green-button-disabled-not-connected"
+                style={styleTextSize}
+            >
                 {props.text}
             </span>
         )
